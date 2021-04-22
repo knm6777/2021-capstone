00f58df963f83b9854434edbe244d2d1fb057ee9
ji-yeon224 <shjh0107@gmail.com>
2021-04-22 23:30:46 +0900

    210422 상품리뷰 가져오기

--- src/main/java/com/example/demo/model/review/ReviewAbs.java
+++ src/main/java/com/example/demo/model/review/ReviewAbs.java
@@ -15,27 +15,36 @@ import java.time.LocalDateTime;
 @IdClass(ReviewPK.class)
 public abstract class ReviewAbs {
 
+    //리뷰번호
     @Id
     @Column(name = "review_no")
     private int reviewNo;
 
+    //상품번호
     @Id
     @Column(name = "pd_no")
     private int pdNo;
 
+    //중간카테
     @Id
     private String subcateNo;
 
+    //큰카테
     @Id
     @Column(name="category_no")
     private String categoryNo;
 
+    //별점
     private int star;
 
+    //리뷰내용
     private String review;
 
+    //고객아이디
     private String customerId;
 
+    //등록날짜 
+    // db에 들어가있는 형식이 날짜타입이랑 안맞아서 string으로 함
     private String reviewDate;
 
 
