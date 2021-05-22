bcafdefb78af5b149fc452b21f18ece7571e4228
ji-yeon224 <shjh0107@gmail.com>
2021-05-22 16:48:42 +0900

    210522 임포트 경로 수정

--- src/main/java/com/example/demo/service/PurchaseService.java
+++ src/main/java/com/example/demo/service/PurchaseService.java
@@ -1,7 +1,7 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Purchase;
-import com.example.demo.repository.PurchaseRepository;
+import com.example.demo.model.user.Purchase;
+import com.example.demo.repository.user.PurchaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
